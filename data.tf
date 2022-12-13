131029afe608d3ffc09a5375b3a3756a8b43b991
Jamie Taffurelli <jltaffurelli@outlook.com>
2022-12-13 11:22:00 +0000

    initial module upload

---
+++ data.tf
@@ -0,0 +1,5 @@
+/*data "azurerm_log_analytics_workspace" "logs" {
+  provider = azurerm.${var.custom_provider}
+  name                = var.log_analytics_workspace.name
+  resource_group_name = var.log_analytics_workspace.resource_group_name
+}*/
---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "storage_account_name" {
+  type        = string
+  description = "The name of the storage account to store state files"
+}
+
+variable "location" {
+  type        = string
+  description = "The location of the storage account to store state files"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group of the storage account to store state files"
+}
+
+variable "containers" {
+  type        = list(string)
+  description = "The storage account containers to store state files for each subscription"
+}
+
+variable "storage_account_network_rules" {
+  type = object(
+    {
+      default_action             = optional(string, "Deny")
+      ip_rules                   = optional(list(string), [])
+      virtual_network_subnet_ids = optional(list(string), [])
+    }
+  )
+  default     = {}
+  description = "The Storage Account firewall rules"
+}
+
+/*variable "log_analytics_workspace" {
+  type = object(
+    {
+      name                = string
+      resource_group_name = string
+    }
+  )
+  description = "The existing log analytics workspaces to send diagnostic logs to"
+}*/
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to resource group and storage account"
+}
