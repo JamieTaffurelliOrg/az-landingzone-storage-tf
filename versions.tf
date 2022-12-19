5b817496cc25cb1ecc6f1a23d31f6e6db8581137
Jamie Taffurelli <jltaffurelli@outlook.com>
2022-12-19 18:30:54 +0000

    added providers (temp workaround)

--- versions.tf
+++ versions.tf
@@ -8,21 +8,3 @@ terraform {
   }
   required_version = "~> 1.3.0"
 }
-
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = true
-    }
-  }
-}
-
-provider "azurerm" {
-  alias = "logs"
-
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = true
-    }
-  }
-}
