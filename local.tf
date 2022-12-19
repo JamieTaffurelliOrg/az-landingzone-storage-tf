93038e4ed9feb9d1881ee02eb09830dc2c7dd860
Jamie Taffurelli <jltaffurelli@outlook.com>
2022-12-19 18:24:34 +0000

    removed plans

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 locals {
   defender_for_cloud_plans = [
-    "AppServices", "ContainerRegistry", "KeyVaults", "KubernetesService", "SqlServers", "SqlServerVirtualMachines", "StorageAccounts", "VirtualMachines", "Arm", "OpenSourceRelationalDatabases", "Containers", "Dns"
+    "AppServices", "ContainerRegistry", "KeyVaults", "KubernetesService", "SqlServers", "SqlServerVirtualMachines", "Arm", "OpenSourceRelationalDatabases", "Containers", "Dns"
   ]
   defender_for_cloud_sub_plans = [
     {
